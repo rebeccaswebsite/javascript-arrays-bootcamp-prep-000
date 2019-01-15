9eb0aaaea6787545d5b023aa553c34124c85f6d1
rebecca_huseyin <rebecca_huseyin@outlook.com>
2019-01-15 21:33:39 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'] ;
+var newCandy = "foo";
+ 
+function addElementToBeginningOfArray(chocolateBars, newCandy){
+  return [newCandy, ...chocolateBars];
+}
+ 
+function destructivelyAddElementToBeginningOfArray(chocolateBars, newCandy){
+  chocolateBars.unshift(newCandy);
+  return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, newCandy){
+  return [...chocolateBars, newCandy];
+}
+ 
+function destructivelyAddElementToEndOfArray(chocolateBars, newCandy){
+  chocolateBars.push(newCandy);
+  return chocolateBars;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
